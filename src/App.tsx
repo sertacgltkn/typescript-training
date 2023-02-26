fc2c97f3caa20f581f7f79849787dd34e35bb13e
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-02-26 13:49:52 +0300

    ts training

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let name: string | number = "Sertaç";
+  let age: number = 30;
+
+  type Obj = {
+    name : string,
+    age : number,
+    isMarried : boolean
+  }
+
+  let obj : Obj = {
+    name : "Sertaç",
+    age : 30,
+    isMarried : false
+  }
+  
+  return <div className="App">{name}</div>;
 }
 
 export default App;
